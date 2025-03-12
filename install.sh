c6561097182a422419b26c67c5cd30e6fd59f84b
Joseph Marbella <dieptuber@gmail.com>
2025-03-12 02:05:10 -0700

    first commit

---
+++ install.sh
@@ -0,0 +1,34 @@
+sudo pacman -Syu
+
+sudo pacman -S pavucontrol \
+  pulseaudio \
+  kitty \
+  base-devel \
+  fastfetch \
+  python-pywal \
+  swww \
+  waybar \
+  hyprland \
+  swaync \
+  starship \
+  hypridle \
+  hyprpicker \
+  hyprshot \
+  hyprlock \
+  wlogout \
+  fd \
+  nwg-look \
+  qogir-icon-theme \
+  materia-gtk-theme \
+  nemo \
+  btop \
+  pipewire \
+  pipewire-pulse \
+  pipewire-alsa \
+  pipewire-jack \
+  pavucontrol \
+  pulsemixer \
+  nm-connection-editor \
+  network-manager-applet
+
+mkdir -p "$HOME/wallpapers/walls"
