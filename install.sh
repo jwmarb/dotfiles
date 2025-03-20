0c28e908c6575723ce04219df8364c12fba644a8
Joseph Marbella <dieptuber@gmail.com>
2025-03-20 10:22:24 -0700

    refactor: setup script

--- install.sh
+++ install.sh
@@ -36,36 +36,4 @@ paru -Sy \
   $DISPLAY_MANAGER \
   $MISC
 
-hyprland & 
-
-while [[ ! -f "$CONFIG_PATH"]]; do
-  sleep 1
-done
-  
-cp -r ./wallpapers "$HOME/"
-cp -r .cache/* ~/.cache/
-touch ~/.cache/wal/colors-kitty.conf
-mkdir -p "$HOME/.config"
-mkdir -p "$HOME/.config/kitty"
-mkdir -p "$HOME/.cache/wal"
-
-echo "[Theme]
-Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
-echo "[General]
-InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
-echo 'eval $(starship init bash)' >> $HOME/.bashrc
-echo 'pokemon-colorscripts -r' >> $HOME/.bashrc
-
-for folder in "$(pwd)/.config"/*; do
-  folder=$(basename $folder)
-  if [[ -e "$HOME/.config/$folder" ]]; then
-    rm -rf "$HOME/.config/$folder"
-  fi
-  ln -s "$(pwd)/.config/$folder" "$HOME/.config/$folder"
-done
-
-sudo systemctl enable sddm
-
-echo "POST-INSTALL INSTRUCTIONS:"
-echo " - Press Alt + W and select the wallpaper to initialize the widgets and color theme"
-echo " - Run \"nwg-look\" to configure the theme"
+hyprland
